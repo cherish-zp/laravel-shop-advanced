518c3c5b8d7f0c4b83e8de8c0500536547d15ec9
vagrant <vagrant@homestead>
2019-01-04 17:15:57 +0000

    商品后台添加类目字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price'];
+    protected $fillable = ['title','category_id', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price'];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
