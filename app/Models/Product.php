5aea8bb39fd851ff062efbdd989633a744ea31c1
vagrant <vagrant@homestead>
2019-01-05 14:52:56 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['title','category_id', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price'];
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
+    protected $fillable = ['title','category_id', 'description', 'image', 'on_sale', 'rating'
+        , 'sold_count', 'review_count', 'price','type'];
+
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
@@ -29,5 +38,10 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
